dc98c830f4fd5709a891b57aa4da5fbcc1b4778e
gudiaabhagat <bhagat.komal55@gmail.com>
2017-01-13 12:06:57 -0800

    Initial commit

---
+++ app/src/main/java/com/example/dell/myfirstapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.dell.myfirstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class firstClass{
+
+      public static void main(String[] args)
+      {
+
+
+          System.out.print("hello);
+
+
+
+      }
+}
